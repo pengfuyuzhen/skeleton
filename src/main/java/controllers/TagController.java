baec4e505f5f9edb90e4cbce5239b65f9cf6e4f9
pengfuyuzhen <pfyz2011@gmail.com>
2017-09-02 14:52:36 -0400

    Refactored PUT tags by just receiving receiptId as an Int with no KV pair

--- src/main/java/controllers/TagController.java
+++ src/main/java/controllers/TagController.java
@@ -1,11 +1,9 @@
 package controllers;
 
-import api.CreateTagRequest;
 import api.ReceiptResponse;
 import dao.TagDao;
 import generated.tables.records.ReceiptsRecord;
 
-import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -23,11 +21,11 @@ public class TagController {
 
     @PUT
     @Path("/{tag}")
-    public void toggleTag(@PathParam("tag") String tagLabel, @NotNull CreateTagRequest tag) {
-        if (tags.isReceiptTagged(tagLabel, tag.receiptId)) {
-            tags.remove(tagLabel, tag.receiptId);
+    public void toggleTag(@PathParam("tag") String tagLabel, int receiptId) {
+        if (tags.isReceiptTagged(tagLabel, receiptId)) {
+            tags.remove(tagLabel, receiptId);
         } else {
-            tags.insert(tagLabel, tag.receiptId);
+            tags.insert(tagLabel, receiptId);
         }
     }
 
